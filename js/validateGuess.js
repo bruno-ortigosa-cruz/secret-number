c6d731b17cf0c5a7f1afe1ef7a1b7ff0af4e1734
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2023-06-17 19:48:51 -0300

    Reversed update

--- js/validateGuess.js
+++ js/validateGuess.js
@@ -2,8 +2,6 @@ const main = document.getElementById("main-container")
 
 function validateGuess(guess) {
     const number = +guess
-    guess = guess.toLowerCase()
-    console.log(guess)
 
     if (guess == "game over") {
         main.innerHTML = `
